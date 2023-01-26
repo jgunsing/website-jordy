fa8680ecd81d7d4eb8f578b5f49023b3a67e2275
jgunsing <j.gunsing@protonmail.com>
2023-01-26 22:02:37 +0100

    fixes

---
+++ resources/js/website.js
@@ -0,0 +1,16 @@
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+function scrolldivOvermij() {
+    var elem = document.getElementById("overmij");
+    elem.scrollIntoView();
+}
+function scrolldivOpleiding() {
+    var elem = document.getElementById("opleidingen");
+    elem.scrollIntoView();
+}
+function scrolldivErvaring() {
+    var elem = document.getElementById("werkervaring");
+    elem.scrollIntoView();
+}
